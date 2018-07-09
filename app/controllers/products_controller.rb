ce1afae763d124d8224e2a6b88a950301823c0b3
tom <levanchi74@gmail.com>
2018-07-09 21:37:26 +0700

    10h thu 2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-    before_action :set_product, only: [:edit, :update, :show, :destory]
+    before_action :set_product, only: [:edit, :update, :show, :destroy]
 
     def index
         @products=Product.all
@@ -9,14 +9,17 @@ class ProductsController < ApplicationController
     end
      
     def new 
+        get_category
         @product=Product.new
+        
     end
 
     def edit
     end
     
     def create
-        @product=Product.new(product_params)
+        @category=Category.find(product_params[:category_id])
+        @product=@category.products.create(product_params)
         if @product.save
             redirect_to products_path
         else 
@@ -25,13 +28,26 @@ class ProductsController < ApplicationController
     end
 
     def update
+        if @product.update(product_params)
+            redirect_to products_path
+        else
+            render 'edit'
+        end
     end
 
     def destroy
+        @product.destroy 
+        redirect_to products_path
     end
 
     private
+    def get_category
+        @categorys=Category.all
+    end
     def set_product
         @product=Product.find(param[:id])
+    end
+    def product_params 
+        params.require(:product).permit(:name,:price,:decription,:category_id)
     end
 end
